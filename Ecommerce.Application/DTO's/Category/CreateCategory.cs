a4383ad5142b8558df83bfb6d42426986fb61584
Codecox-pl <patricklmo32@gmail.com>
2026-06-02 18:34:16 -0500

    Add Previous Dto Category

---
+++ Ecommerce.Application/DTO's/Category/CreateCategory.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecommerce.Application.DTO_s.Category
+{
+    public class CreateCategory
+    {
+
+    }
+}
---
+++ Ecommerce.Application/DTO's/Category/UpdateCategory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecommerce.Application.DTO_s.Category
+{
+    public class UpdateCategory
+    {
+        public int Id { get; set; }
+
+        public string? Name { get; set; }
+
+        public string? Description { get; set; }
+
+        public bool IsActive { get; set; } = true;
+
+        public int? ParentCategoryId { get; set; }
+    }
+}
